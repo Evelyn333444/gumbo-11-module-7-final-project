da6354d1172433b5a03666864ce4c3cbdabde421
Evelyn Sallee Brackebusch <brackesb12@gmail.com>
2026-04-12 11:14:45 -0400

    third push

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import MovieCard from './components/MovieCard';
-import MovieModal from './components/MovieModal';
-import Sort from './components/Sort';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import MovieModal from "./components/MovieModal";
+import Sort from "./components/Sort";
 
-
-const API_URL = 'https://omdbapi.com/?apikey=fe2f6c44';
+const API_URL = "https://omdbapi.com/?apikey=fe2f6c44";
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [sortType, setSortType] = useState('');
-    const [viewModal, setViewModal] = useState(false)
-    const [movie, setMovie] = useState({})
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortType, setSortType] = useState("");
+  const [viewModal, setViewModal] = useState(false);
+  const [movie, setMovie] = useState({});
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        setMovies(data.Search);
-        setMovies(data.Search.length > 0 ? data.Search : [])
-    }
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search || []);
+  };
 
-    return (
-        <div className="app">
-            <h1>Movie Center</h1>
+  const sortedMovies = movies ? [...movies] : [];
+  if (sortType === "NEW_TO_OLD") {
+    sortedMovies.sort((a, b) => Number(b.Year) - Number(a.Year));
+  } else if (sortType === "OLD_TO_NEW") {
+    sortedMovies.sort((a, b) => Number(a.Year) - Number(b.Year));
+  }
 
-            <div className="search">
-                <input
-                    placeholder="Search for Movies"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                    src="https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
-                    alt="search icon"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
+  return (
+    <div className="app">
+      <h1>Movie Center</h1>
 
-            {sortedMovies?.length > 0 ? (
-                <div className="container">
-                    {sortedMovies.map((movie) => (
-                        <MovieCard movie={movie} key={movie.imdbID} onClick={() => {
-                            setMovie(movie)
-                            setViewModal(true)
-                        }} />
-                    ))}
-                </div>
-            ) : (
-                <div className="empty">
-                    <h2>No Movies found</h2>
-                </div>
-            )}
-            {viewModal && (<MovieModal movie={movie} onClose={()=> setViewModal(false)} />)}
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
+          alt="search icon"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+<Sort sortType={sortType} setSortType={setSortType} />
+
+      {sortedMovies?.length > 0 ? (
+        <div className="container">
+          {sortedMovies.map((movie) => (
+            <MovieCard
+              movie={movie}
+              key={movie.imdbID}
+              onClick={() => {
+                setMovie(movie);
+                setViewModal(true);
+              }}
+            />
+          ))}
         </div>
-//Lines 55-70 are showing how to put the modal in action on the website.
-
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+        )}
+      {viewModal && (
+        <MovieModal movie={movie} onClose={() => setViewModal(false)} />
+      )}
+    </div>
+    //Lines 55-70 are showing how to put the modal in action on the website.
+  );
+};
 
 export default App;
-
-
-
-  
