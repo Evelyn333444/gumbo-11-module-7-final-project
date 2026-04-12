b0779ce69c0d3ce3bc441f665b1045171eb1651e
Evelyn Sallee Brackebusch <brackesb12@gmail.com>
2026-04-12 10:28:15 -0400

    third push

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,19 +1,15 @@
-const sortedMovies = [...movies]; {
-    if (sortType === 'NEW_TO_OLD') {
-        sortedMovies.sort((a, b) => Number(b.Year) - Number(a.Year));
-    } else if (sortType === 'OLD_TO_NEW') {
-        sortedMovies.sort((a, b) => Number(a.Year) - Number(b.Year));
-    }
+import React from "react";
 
-    return (
-         <div className="filter-container">
-                <select value={sortType} onChange={(e) => setSortType(e.target.value)}>
-                    <option value="">Sort by...</option>
-                    <option value="NEW_TO_OLD">Newest to Oldest</option>
-                    <option value="OLD_TO_NEW">Oldest to Newest</option>
-                </select>
-            </div>
-    );
-} 
+const Sort = ({ sortType, setSortType }) => {
+  return (
+    <div className="filter-container">
+      <select value={sortType} onChange={(e) => setSortType(e.target.value)}>
+        <option value="">Sort by...</option>
+        <option value="NEW_TO_OLD">Newest to Oldest</option>
+        <option value="OLD_TO_NEW">Oldest to Newest</option>
+      </select>
+    </div>
+  );
+};
 
-export default Sort
+export default Sort;
